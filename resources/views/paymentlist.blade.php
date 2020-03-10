bb332fcfabde83db189e65b3d198b5ac189b802a
Gerasimov1991 <svt.triya1991@gmail.com>
2020-03-10 16:39:09 +1000

    some

--- resources/views/paymentlist.blade.php
+++ resources/views/paymentlist.blade.php
@@ -93,7 +93,7 @@
 			<fieldset>
 				<div class="row">
                     <div class="col-md-12">
-                        <h3>Billing details aaa</h3>
+                        <h3>Billing details</h3>
                     </div>
 					<div class="col-md-6">
                         <div>                            
